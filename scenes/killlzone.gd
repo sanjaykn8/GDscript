b53bcb764e0c238e3e93cb9f877f8cee8cfba0f0
sanjaykn8 <nisanir8@gmail.com>
2025-08-24 15:33:32 +0530

    fix crash

--- scenes/killlzone.gd
+++ scenes/killlzone.gd
@@ -3,7 +3,12 @@ extends Area2D
 @onready var timer = $Timer
 
 func _on_body_shape_entered(body_rid: RID, body: Node2D, body_shape_index: int, local_shape_index: int) -> void:
-	timer.start()
+	if body.name == "player1" or body.name == "player2":
+		Engine.time_scale = 0.5
+		# instead of Timer.start(), use SceneTreeTimer that ignores time scale
+		var t = get_tree().create_timer(0.5, false, true) # (time, process_in_physics, ignore_time_scale)
+		t.timeout.connect(_on_timer_timeout)
 	
 func _on_timer_timeout():
+	Engine.time_scale = 1
 	get_tree().reload_current_scene()
