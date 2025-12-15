c26a8a8fb81877fb672eb8f12ddf75eff9603765
sanjaykn8 <nisanir8@gmail.com>
2025-12-15 22:45:16 +0530

    bgm

--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -4,7 +4,7 @@ extends Area2D
 
 func _on_body_shape_entered(body_rid: RID, body: Node2D, body_shape_index: int, local_shape_index: int) -> void:
 	if body.name == "player1" or body.name == "player2":
-		Engine.time_scale = 0.1
+		Engine.time_scale = 0.3
 		# instead of Timer.start(), use SceneTreeTimer that ignores time scale
 		var t = get_tree().create_timer(0.5, false, true) # (time, process_in_physics, ignore_time_scale)
 		t.timeout.connect(_on_timer_timeout)
